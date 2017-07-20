3cb33999c24e2046ce2c31cdeb1fb7cbf5a9072e
Duc Nghiem Xuan <xuanduc987@gmail.com>
2017-07-20 23:04:23 +0900

    Update buildpack-boot.sh

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -2,4 +2,6 @@ export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
 if ! [ -L "/app/mono" ]; then
   ln -s vendor/mono /app
+  # Certs sync
+  /app/mono/bin/cert-sync --user /etc/ssl/certs/ca-certificates.crt
 fi
